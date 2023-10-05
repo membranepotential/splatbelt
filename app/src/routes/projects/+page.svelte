b7e66a8cd14bb79253e905ed01cea01de4a4e7af
Torsten Rüter <torsten@rueter.net>
2023-10-05 14:52:04 +0200

    Rain some App UI on the layout

--- app/src/routes/projects/+page.svelte
+++ app/src/routes/projects/+page.svelte
@@ -27,14 +27,27 @@
 	}
 </script>
 
-<ul>
-	{#each data.projects as project}
-		<li class="h-10 flex flex-row items-center">
-			<a class="grow" href="/project/{project.id}">{project.name}</a>
-			<button class="ml-2 text-red-600" on:click={() => deleteProject(project.id)}>Delete</button>
-		</li>
-	{/each}
-	<li class="h-10 flex flex-row items-center">
-		<button on:click={createProject}>New Project</button>
-	</li>
-</ul>
+<div class="py-10">
+	<header>
+		<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+			<h1 class="text-3xl font-bold leading-tight tracking-tight text-gray-900">Projects</h1>
+		</div>
+	</header>
+	<main>
+		<div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+			<ul>
+				{#each data.projects as project}
+					<li class="h-10 flex flex-row items-center">
+						<a class="grow" href="/project/{project.id}">{project.name}</a>
+						<button class="ml-2 text-red-600" on:click={() => deleteProject(project.id)}
+							>Delete</button
+						>
+					</li>
+				{/each}
+				<li class="h-10 flex flex-row items-center">
+					<button on:click={createProject}>New Project</button>
+				</li>
+			</ul>
+		</div>
+	</main>
+</div>

05f5a4e9887700f9efec1ff0c661e2943bdaf5a4
Torsten Rüter <torsten@rueter.net>
2023-10-05 15:04:21 +0200

    Update project list UI

--- app/src/routes/projects/+page.svelte
+++ app/src/routes/projects/+page.svelte
@@ -35,28 +35,47 @@
 	</header>
 	<main>
 		<div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-			<ul>
+			<ul role="list" class="divide-y divide-gray-100">
 				{#each data.projects as project}
-					<li class="h-10 flex flex-row items-center">
-						<a class="grow" href="/project/{project.id}">{project.name}</a>
+					<li
+						v-for="project in projects"
+						:key="project.id"
+						class="flex items-center justify-between gap-x-6 py-5"
+					>
+						<div class="min-w-0">
+							<div class="flex items-start gap-x-3">
+								<p class="text-sm font-semibold leading-6 text-gray-900">{project.name}</p>
+								<!-- <p class="rounded-md whitespace-nowrap mt-0.5 px-1.5 py-0.5 text-xs font-medium ring-1 ring-inset">{project.status }</p> -->
+							</div>
+							<div class="mt-1 flex items-center gap-x-2 text-xs leading-5 text-gray-500">
+								<p class="truncate">Created {project.created}</p>
+							</div>
+						</div>
+						<div class="flex flex-none items-center gap-x-4">
+							<a
+								href="/project/{project.id}"
+								class="hidden rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:block"
+								>View project<span class="sr-only" /></a
+							>
 
-						<button
-							type="button"
-							on:click={() => deleteProject(project.id)}
-							class="rounded-full bg-red-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
-							>Delete</button
-						>
+							<button
+								type="button"
+								on:click={() => deleteProject(project.id)}
+								class="rounded-full bg-red-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+								>Delete</button
+							>
+						</div>
 					</li>
 				{/each}
-				<li class="h-10 flex flex-row items-center">
-					<button
-						type="button"
-						on:click={createProject}
-						class="rounded-full bg-green-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
-						>Create New Project</button
-					>
-				</li>
 			</ul>
+			<div class="mt-4">
+				<button
+					type="button"
+					on:click={createProject}
+					class="rounded-full bg-green-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600"
+					>Create New Project</button
+				>
+			</div>
 		</div>
 	</main>
 </div>

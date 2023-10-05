f483c7306119589481db8bb8b8df61791d3b1a59
Torsten Rüter <torsten@rueter.net>
2023-10-05 15:08:07 +0200

    Apply prettier rules to adjacent files

--- app/src/routes/project/[id]/+page.ts
+++ app/src/routes/project/[id]/+page.ts
@@ -1,11 +1,11 @@
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch, params }) => {
-    try {
-        const res = await fetch(`/api/projects/${params.id}`);
-        return { project: res.json() }
-    } catch (err) {
-        console.error(err);
-        throw err;
-    }
+	try {
+		const res = await fetch(`/api/projects/${params.id}`);
+		return { project: res.json() };
+	} catch (err) {
+		console.error(err);
+		throw err;
+	}
 };

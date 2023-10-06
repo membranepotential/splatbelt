49e763861228a0f9a054a147e23b0a1faf055aee
Torsten Rüter <torsten@rueter.net>
2023-10-06 06:09:26 +0200

    Tabs (#8)
    * Initial Tab layout for projects
    
    * Wrap up Tab implementation

---
+++ app/src/routes/project/[id]/timeline/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import type { PageData } from './$types';
+  import type { Project } from '$lib/models/project';
+
+  export let data: PageData;
+  const project: Project = data.project;
+  console.log('Project', project);
+</script>
+
+<div class="py-10">Canvas</div>

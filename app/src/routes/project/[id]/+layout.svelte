cb0011610076b1c143c79603987d546e05408186
Torsten Rüter <torsten@rueter.net>
2023-10-06 06:09:26 +0200

    Tabs (#8)
    * Initial Tab layout for projects
    
    * Wrap up Tab implementation

---
+++ app/src/routes/project/[id]/+layout.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  import type { LayoutData } from './$types';
+  import type { Project } from '$lib/models/project';
+  import { page } from '$app/stores';
+  import { derived } from 'svelte/store';
+
+  export let data: LayoutData;
+  const project: Project = data.project;
+
+  const tabs = [
+    { id: 'info', label: 'Info' },
+    { id: 'timeline', label: 'Timeline' },
+  ];
+
+  page.subscribe((value) => {
+    console.log(value);
+  });
+  const activeTab = derived(page, (page) => page.route.id?.split('/').pop());
+  console.log(activeTab);
+
+  const classesActive = 'bg-indigo-100 text-indigo-700';
+  const classesInactive = 'text-gray-500 hover:text-gray-700';
+</script>
+
+<div class="py-10">
+  <header class="mb-8">
+    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <h1 class="text-3xl font-bold leading-tight tracking-tight text-gray-900">
+        {project.name}
+      </h1>
+    </div>
+  </header>
+  <main>
+    <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+      <nav class="flex space-x-4" aria-label="Tabs">
+        <a
+          href="/project/{project.id}/info"
+          class="rounded-md px-3 py-2 text-sm font-medium {$activeTab === 'info'
+            ? classesActive
+            : classesInactive}"
+          aria-current="page">Info</a
+        >
+        <a
+          href="/project/{project.id}/timeline"
+          class="rounded-md px-3 py-2 text-sm font-medium {$activeTab ===
+          'timeline'
+            ? classesActive
+            : classesInactive}">Timeline</a
+        >
+      </nav>
+
+      <main class="mx-auto max-w-7xl">
+        <slot />
+      </main>
+    </div>
+  </main>
+</div>

49b0db8bb5418d2a440087fc18a2f39d681eac52
Torsten Rüter <torsten@rueter.net>
2023-10-06 07:36:11 +0200

    Add Threejs example to project

--- app/src/routes/project/[id]/timeline/+page.svelte
+++ app/src/routes/project/[id]/timeline/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   import type { PageData } from './$types';
   import type { Project } from '$lib/models/project';
+  import Timeline from './Timeline.svelte';
 
   export let data: PageData;
   const project: Project = data.project;
   console.log('Canvas for project', project);
 </script>
 
-<div class="py-10">Canvas</div>
+<div class="py-10"><Timeline /></div>
---
+++ app/src/routes/project/[id]/timeline/Timeline.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { Canvas } from '@threlte/core';
+  import Scene from './Scene.svelte';
+
+  /**
+   * Example from https://github.com/iansamz/threlte6-spinning-ball/tree/main
+   */
+</script>
+
+<div class="sphere-canvas">
+  <Canvas>
+    <Scene />
+  </Canvas>
+</div>
+
+<style>
+  .sphere-canvas {
+    width: 100%;
+    height: 600px;
+  }
+</style>

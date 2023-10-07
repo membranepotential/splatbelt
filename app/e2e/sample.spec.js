4af014c70e24fcc08052ca7286b2c434903424d6
Torsten Rüter <torsten@rueter.net>
2023-10-07 07:30:56 +0200

    Fix E2E test

--- app/e2e/sample.spec.js
+++ app/e2e/sample.spec.js
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test'
 
 test('index page has expected h1', async ({ page }) => {
-  // Expect that page redirects to other url in playwright
+  await page.goto('/')
   await expect(page.getByRole('link', { name: 'Projects' })).toBeVisible()
   await expect(page.getByRole('link', { name: 'Projectsx' })).toBeVisible({
     visible: false,

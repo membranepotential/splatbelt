c841e2b243c609fb74ec1d63d0097801e0f420ac
Radu Gota <radugota@Radus-MacBook-Pro.local>
2023-12-02 04:47:39 +0100

    Added Shot settings menu ani

--- app/src/routes/app/components/ShotSettings.svelte
+++ app/src/routes/app/components/ShotSettings.svelte
@@ -1,5 +1,16 @@
+<script>
+  import { app } from '$lib/stores'
+  import { VIEWER_STATE } from '$lib/types'
+
+  let settingsOpen = false
+
+  function toggleSettings() {
+    settingsOpen = !settingsOpen
+  }
+</script>
+
 <div class="shot-settings-container w-100 flex overflow-hidden">
-  <div class="duration w-/12 mr-6 p-4">
+  <div class="duration mr-6 w-10/12 p-4">
     <h5 class="mb-2 text-white">Speed</h5>
     <span class="isolate inline-flex rounded-md shadow-sm">
       <button
@@ -10,7 +21,7 @@
       <button
         type="button"
         class="text-md relative -ml-px inline-flex items-center bg-transparent px-3 py-2 font-semibold text-gray-900 text-indigo-300 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-        >0.75</button
+        >0.7</button
       >
       <button
         type="button"
@@ -19,55 +30,17 @@
       >
       <button
         type="button"
-        class="text-md relative -ml-px inline-flex items-center rounded-r-md bg-transparent px-3 py-2 font-semibold text-gray-900 text-indigo-300 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+        class=" text-md relative -ml-px inline-flex items-center bg-transparent px-3 py-2 font-semibold text-gray-900 text-indigo-300 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
         >x2</button
       >
-    </span>
-  </div>
-  <div class=" mr-2 w-2/12 py-4 text-center">
-    <h5 class="mb-2 text-white">Redo</h5>
-    <span class="isolate inline-flex rounded-md shadow-sm">
       <button
         type="button"
-        class="text-md font-semiboldbg-slate-950 relative inline-flex items-center rounded-md bg-slate-200 bg-transparent px-3 py-2 text-indigo-300 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-      >
-        <svg
-          width="24px"
-          height="24px"
-          stroke-width="1.5"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          color="#000000"
-          ><path
-            d="M21.1679 8C19.6247 4.46819 16.1006 2 11.9999 2C6.81459 2 2.55104 5.94668 2.04932 11"
-            stroke="#000000"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          /><path
-            d="M17 8H21.4C21.7314 8 22 7.73137 22 7.4V3"
-            stroke="#000000"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          /><path
-            d="M2.88146 16C4.42458 19.5318 7.94874 22 12.0494 22C17.2347 22 21.4983 18.0533 22 13"
-            stroke="#000000"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          /><path
-            d="M7.04932 16H2.64932C2.31795 16 2.04932 16.2686 2.04932 16.6V21"
-            stroke="#000000"
-            stroke-width="1.5"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          /></svg
-        ></button
+        class="text-md relative -ml-px inline-flex items-center rounded-r-md bg-transparent px-3 py-2 font-semibold text-gray-900 text-indigo-300 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
+        >x4</button
       >
     </span>
   </div>
+
   <div class=" w-2/12 py-4">
     <h5 class="mb-2 text-white">Delete</h5>
     <span class="isolate inline-flex rounded-md shadow-sm">
@@ -107,10 +80,13 @@
 <style lang="sass">
 .shot-settings-container
   position: absolute
-  bottom: 0
+  bottom: -13vh
   width: 100%
   height: 13vh
   @apply bg-slate-950
+
+  &.open
+    transform: translateX(13vh)
   button
     &.active
       @apply bg-slate-200 text-indigo-700

b5d939f82f949f4531645f3f759d94cdc9a44000
Torsten Rüter <torsten@rueter.net>
2023-12-13 21:29:32 +0100

    Fix error screen

--- app/src/routes/+error.svelte
+++ app/src/routes/+error.svelte
@@ -6,6 +6,6 @@
   <title>Error</title>
 </svelte:head>
 
-<div class="mx-auto my-4 max-w-md">
+<div class="mx-auto max-w-md px-4 py-2">
   <h1>{$page.status}: {$page.error?.message}</h1>
 </div>
--- app/src/routes/[id=integer]/model.ply/+page.server.ts
+++ app/src/routes/[id=integer]/model.ply/+page.server.ts
@@ -9,6 +9,6 @@ export async function load({ params }) {
   if (await exists(params.id)) {
     throw redirect(302, await getDownloadUrl(params.id))
   } else {
-    throw error(404, 'Not found')
+    throw error(404, 'Not Found')
   }
 }
